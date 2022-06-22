5823f39c04e624dd1ebdbc965300704f45a1ec46
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2022-06-22 14:16:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
-/*  Timer interrupt example
-*   Operate an Red LED using an timer interrupt. 
+/* Time triggered interrupt
+*  ========================
 *
-*   Board: NUCLEO L476RG
-*   Author: Dr Craig Evans
-*   Editied: Andrew Knowles 2022
-*   MBED OS: 6.14
-*/ 
+*  Function:               Preform an interrupt at a timed interval
+*  Circuit Schematic No.:  2 : https://github.com/ELECXJEL2645/Circuit_Schematics
+*  Required Libraries:     
+*
+*  Authored by:            Dr Tim Amsdon
+*  Date:                   2022
+*  Collaberators:          Andrew Knowles
+*  Version:                1.0
+*  Revision Date:          06/2022 
+*  MBED Studio Version:    1.4.1
+*  MBED OS Version:        6.12.0
+*  Board:	                NUCLEO L476RG */
  
 #include "mbed.h"
  
