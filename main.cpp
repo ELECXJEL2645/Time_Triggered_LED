094a4bcbbdd4cd903b9ede88716fe35538b1a200
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2022-06-22 14:28:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-/* Time triggered interrupt
-*  ========================
+/* Time_Triggered_LED
+*  ==================
 *
 *  Function:               Preform an interrupt at a timed interval
 *  Circuit Schematic No.:  2 : https://github.com/ELECXJEL2645/Circuit_Schematics
@@ -12,7 +12,7 @@
 *  Revision Date:          06/2022 
 *  MBED Studio Version:    1.4.1
 *  MBED OS Version:        6.12.0
-*  Board:	                NUCLEO L476RG */
+*  Board:	                 NUCLEO L476RG */
  
 #include "mbed.h"
  
